b30c7f3f2a4846838e1e6ac50f059e2fc096ca09
ahmed mohamed <ahmedmohamed26.github.io>
2021-08-14 22:47:54 +0200

    add dashboard page

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -6,10 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  loadingSpinner: boolean;
+  filter: any = {};
+  items = [
+    {
+      'cityName': 'nasr city',
+      'countPerson': 1000,
+      'countPlaces':500
+    },
+    {
+      'cityName': 'maadi',
+      'countPerson': 1000,
+      'countPlaces':500
+    },
+    {
+      'cityName': '6 october',
+      'countPerson': 1000,
+      'countPlaces':500
+    },
+    {
+      'cityName': 'helwan',
+      'countPerson': 1000,
+      'countPlaces':500
+    },
+    {
+      'cityName': 'new cairo',
+      'countPerson': 1000,
+      'countPlaces':500
+    },
 
-  constructor() { }
+  ]
+  constructor() {
+    this.loadingSpinner = true;
+    this.filter = {
+      city: '1',
+      count:'1'
+    }
+   }
 
-  ngOnInit(): void {
+   ngOnInit(): void {
+    setTimeout(() => {
+      this.loadingSpinner = false;
+    }, 1500);
   }
 
 }
